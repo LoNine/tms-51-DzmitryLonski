5d11cdcdb280a311169d826be991670fa0f2737d
Dzmitry Lonski <mylastdayd@gmail.com>
2021-07-23 08:33:11 +0300

    FE-33 Add push method

--- FE-33 Linked List/linked-list.js
+++ FE-33 Linked List/linked-list.js
@@ -10,4 +10,21 @@ class LinkedList {
         this.head = null;
         this.length = 0;
     }
+
+    push(value) {
+        let node = new Node(value);
+
+        if (this.length === 0) {
+            this.head = node;
+        } else {
+            let current = this.head;
+
+            while (current.next) {
+                current = current.next
+            }
+            
+            current.next = node;
+        }
+        this.length++
+    }
 }
